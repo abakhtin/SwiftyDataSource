9766929f92499e62084496a528bfb452a0cbb633
Dima Shelkov <shalkovy@gmail.com>
2020-06-08 15:57:33 +0300

    #Round corners
    Add rounded corners method for ios 11+

--- SwiftyDataSource/SwiftyDataSource/Helpers/UIKitExtensions/UIView+Corners.swift
+++ SwiftyDataSource/SwiftyDataSource/Helpers/UIKitExtensions/UIView+Corners.swift
@@ -9,35 +9,15 @@
 import UIKit
 
 public extension UIView {
-    enum Corner: Int {
-        case bottomRight = 0,
-        topRight,
-        bottomLeft,
-        topLeft
-    }
-    
-    private func round(corner: Corner) -> CACornerMask.Element {
-        let corners: [CACornerMask.Element] = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
-        return corners[corner.rawValue]
-    }
-    
-    private func createMask(corners: [Corner]) -> UInt {
-        return corners.reduce(0, { (a, b) -> UInt in
-            return a + round(corner: b).rawValue
-        })
-    }
-    
-    @available(iOS 11, *)
-    func round(corners: [Corner], byRadius value: CGFloat) {
-        layer.cornerRadius = value
-        let maskedCorners: CACornerMask = CACornerMask(rawValue: createMask(corners: corners))
-        layer.maskedCorners = maskedCorners
-    }
-    
-    
     // Should be called after views layout
+    
     func round(corners: UIRectCorner, byRadius value: CGFloat) {
-        round(corners: corners, byRadii: CGSize(width: value, height: value))
+        if #available(iOS 11, *) {
+            layer.cornerRadius = value
+            layer.maskedCorners = CACornerMask(rawValue: corners.rawValue)
+        } else {
+            round(corners: corners, byRadii: CGSize(width: value, height: value))
+        }
     }
 
     func round(corners: UIRectCorner, byRadii value: CGSize) {
