5d9f0a437d4bedac381b999c6b1b384ca931dd46
Yauheni Fiadotau <e.fedotov@launchoptions.by>
2022-09-28 12:27:18 +0300

    Add Matchable protocol Conform IdentifiableProtocol to Matchable Add replaceObject method with new Matchable object to DataSourceContainer

--- Sources/SwiftyDataSource/Containers/DataSourceContainer+IdentifiableProtocol.swift
+++ Sources/SwiftyDataSource/Containers/DataSourceContainer+IdentifiableProtocol.swift
@@ -9,18 +9,22 @@
 #if os(iOS)
 import Foundation
 
-public protocol IdentifiableProtocol {
+public protocol IdentifiableProtocol: Matchable {
     var id: Int { get }
 }
 
+extension IdentifiableProtocol {
+    static func ~= (lhs: Self, rhs: Self) -> Bool { lhs.id == rhs.id }
+}
+
 extension DataSourceContainer where ResultType: IdentifiableProtocol {
-    public func indexPath(forIdentifiable object: IdentifiableProtocol) -> IndexPath?  {
+    public func indexPath(forIdentifiable object: ResultType) -> IndexPath?  {
         return search { (indexPath, objectInContainer) -> Bool in
             return object.id == objectInContainer.id
         }
     }
 
-    public func objectsInContainer(forIdentifiable object: IdentifiableProtocol) -> [ResultType]  {
+    public func objectsInContainer(forIdentifiable object: ResultType) -> [ResultType]  {
         var objects = [ResultType]()
         enumerate { _, objectInContainer in
             if object.id == objectInContainer.id {
